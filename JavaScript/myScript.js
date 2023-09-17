571bae94d8166ae7df0e834de3592f4bbfd0e9ab
svinxen <aaxelsson@live.com>
2023-09-17 17:00:46 +0200

    new design

---
+++ JavaScript/myScript.js
@@ -0,0 +1,27 @@
+
+/* VISA MENY*/
+const navMenu = document.getElementById('nav-menu'),
+      navToggle = document.getElementById('nav-toggle'),
+      navClose = document.getElementById('nav-close')
+
+
+if(navToggle){
+    navToggle.addEventListener('click', () =>{
+        navMenu.classList.add('show-menu')
+        })
+}
+
+if(navClose){
+    navClose.addEventListener('click', () =>{
+        navMenu.classList.remove('show-menu')
+    })
+}
+
+/* MENY I MOBIL */
+const navLink = document.querySelectorAll('.nav__link')
+
+const linkAction = () =>{
+    const navMenu = document.getElementById('nav-menu') /* Tryck på länk tar bort menyn */
+    navMenu.classList.remove('show-menu')
+}
+navLink.forEach(n => n.addEventListener('click0', linkAction))
